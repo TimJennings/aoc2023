fb02e62c3768d8a5d7dc002e6a86c315290d456b
Tim Jennings <10922362+TimJennings@users.noreply.github.com>
2023-12-09 18:02:19 +0000

    day 7:2, complete refactor to handle wildcards

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,4 +1,7 @@
-use std::cmp::Ordering;
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
 use crate::common::file_io::read_file_to_string;
 
@@ -12,17 +15,28 @@ const HIGH_CARD: u32 = 1;
 
 pub fn puz1() {
     let input = read_file_to_string("input/day7-input");
-    let mut cards = parse_cards(&input);
-    println!("{cards:?}");
-    cards.sort_by(|a, b| a.value.cmp(&b.value).then(a.compare_cards(b)));
+    let cards = parse_cards(&input);
+    let answer = sort_and_score(cards);
+    println!("{answer}");
+}
 
-    println!("{cards:?}");
+pub fn puz2() {
+    let mut input = read_file_to_string("input/day7-input");
+    // replace all the jacks with * so we can use the same code for puz1 and puz2
+    input = input.replace('J', "*");
+    let cards = parse_cards(&input);
+    let answer = sort_and_score(cards);
+    println!("{answer}");
+}
+
+fn sort_and_score(mut cards: Vec<Hand>) -> u32 {
+    cards.sort_by(|a, b| a.value.cmp(&b.value).then(a.compare_cards(b)));
 
     let mut answer = 0;
     for (index, card) in cards.iter().enumerate() {
         answer += ((index as u32 + 1) * card.bid);
     }
-    println!("{answer}");
+    answer
 }
 
 fn card_value(card: char) -> u32 {
@@ -33,11 +47,12 @@ fn card_value(card: char) -> u32 {
         'Q' => 12,
         'K' => 13,
         'A' => 14,
+        '*' => 0, //wildcard
         _ => 0,
     }
 }
 
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Debug)]
 struct Hand {
     hand: Vec<char>,
     bid: u32,
@@ -58,61 +73,46 @@ impl Hand {
     }
 }
 
-fn find_kind(input: &Vec<char>) -> u32 {
-    //copy the input
-    let mut sorted_copy = Vec::from(['0', '0', '0', '0', '0']);
+fn find_kind2(input: &Vec<char>) -> u32 {
+    let mut same_set: HashMap<char, u32> = HashMap::new();
+    let mut jokers = 0;
 
-    sorted_copy.clone_from_slice(input.as_slice());
-
-    sorted_copy.sort_by(|a, b| card_value(*a).cmp(&card_value(*b)));
-
-    let result = HIGH_CARD;
-
-    if sorted_copy.iter().all(|card| card.eq(&sorted_copy[0])) {
-        return FIVE_OF_A_KIND;
-    } else if (sorted_copy[0..4]
-        .iter()
-        .all(|card| card.eq(&sorted_copy[0])))
-        || (sorted_copy[1..5]
-            .iter()
-            .all(|card| card.eq(&sorted_copy[1])))
-    {
-        return FOUR_OF_A_KIND;
-    } else if (sorted_copy[0..3]
-        .iter()
-        .all(|card| card.eq(&sorted_copy[0]))
-        && (sorted_copy[3].eq(&sorted_copy[4])))
-        || (sorted_copy[2..=4]
-            .iter()
-            .all(|card| card.eq(&sorted_copy[2]))
-            && (sorted_copy[0].eq(&sorted_copy[1])))
-    {
-        return FULL_HOUSE;
-    } else if (sorted_copy[0..3]
-        .iter()
-        .all(|card| card.eq(&sorted_copy[0])))
-        || (sorted_copy[1..4]
-            .iter()
-            .all(|card| card.eq(&sorted_copy[1])))
-        || (sorted_copy[2..5]
-            .iter()
-            .all(|card| card.eq(&sorted_copy[2])))
-    {
-        return THREE_OF_A_KIND;
-    } else if (sorted_copy[0].eq(&sorted_copy[1]) && sorted_copy[2].eq(&sorted_copy[3]))
-        || (sorted_copy[0].eq(&sorted_copy[1]) && sorted_copy[3].eq(&sorted_copy[4]))
-        || (sorted_copy[1].eq(&sorted_copy[2]) && sorted_copy[3].eq(&sorted_copy[4]))
-    {
-        return TWO_PAIR;
-    } else if (sorted_copy[0].eq(&sorted_copy[1]))
-        || (sorted_copy[1].eq(&sorted_copy[2]))
-        || (sorted_copy[2].eq(&sorted_copy[3]))
-        || (sorted_copy[3].eq(&sorted_copy[4]))
-    {
-        return ONE_PAIR;
+    for char in input {
+        if char.eq(&'*') {
+            jokers += 1;
+        } else {
+            match same_set.get(char) {
+                Some(count) => same_set.insert(*char, count + 1),
+                None => same_set.insert(*char, 1),
+            };
+        }
     }
 
-    result
+    // apply the jokers then map onto kinds
+    let mut counts: Vec<u32> = same_set.iter().map(|(k, v)| *v).collect();
+
+    // special case all jokers
+    if counts.len() == 0 {
+        return FIVE_OF_A_KIND;
+    }
+
+    // sort highest first
+    counts.sort_by(|a, b| b.cmp(a));
+
+    // use the jokers on the largest set
+    let mut first = counts.first_mut().unwrap();
+    *first += jokers;
+
+    // map onto kinds
+    match counts.as_slice() {
+        [5] => FIVE_OF_A_KIND,
+        [4, 1] => FOUR_OF_A_KIND,
+        [3, 1, 1] => THREE_OF_A_KIND,
+        [3, 2] => FULL_HOUSE,
+        [2, 2, 1] => TWO_PAIR,
+        [2, 1, 1, 1] => ONE_PAIR,
+        _ => HIGH_CARD,
+    }
 }
 
 fn parse_cards(input: &str) -> Vec<Hand> {
@@ -123,7 +123,7 @@ fn parse_cards(input: &str) -> Vec<Hand> {
 
         // calculate value
         let hand = String::from(split[0]).chars().collect();
-        let kind = find_kind(&hand);
+        let kind = find_kind2(&hand);
         hands.push(Hand {
             hand: hand,
             bid: split[1].parse().unwrap(),
@@ -135,17 +135,24 @@ fn parse_cards(input: &str) -> Vec<Hand> {
 
 mod test {
     use crate::day7::{
-        parse_cards, FIVE_OF_A_KIND, FOUR_OF_A_KIND, FULL_HOUSE, ONE_PAIR, THREE_OF_A_KIND,
-        TWO_PAIR,
+        find_kind2, parse_cards, sort_and_score, FIVE_OF_A_KIND, FOUR_OF_A_KIND, FULL_HOUSE,
+        ONE_PAIR, THREE_OF_A_KIND, TWO_PAIR,
     };
-
-    use super::find_kind;
 
     #[test]
     pub fn test_5of() {
         assert_eq!(
             FIVE_OF_A_KIND,
-            find_kind(&Vec::from(['0', '0', '0', '0', '0']))
+            find_kind2(&Vec::from(['*', '*', '*', '*', '*']))
+        );
+        assert_eq!(
+            FIVE_OF_A_KIND,
+            find_kind2(&Vec::from(['0', '0', '0', '0', '0']))
+        );
+
+        assert_eq!(
+            FIVE_OF_A_KIND,
+            find_kind2(&Vec::from(['0', '0', '*', '0', '0']))
         );
     }
 
@@ -153,66 +160,106 @@ mod test {
     pub fn test_4of() {
         assert_eq!(
             FOUR_OF_A_KIND,
-            find_kind(&Vec::from(['0', '0', '0', '0', '1']))
+            find_kind2(&Vec::from(['0', '*', '0', '0', '1']))
         );
         assert_eq!(
             FOUR_OF_A_KIND,
-            find_kind(&Vec::from(['0', '1', '1', '1', '1']))
+            find_kind2(&Vec::from(['0', '*', '1', '1', '1']))
+        );
+
+        assert_eq!(
+            FOUR_OF_A_KIND,
+            find_kind2(&Vec::from(['0', '0', '0', '0', '1']))
+        );
+        assert_eq!(
+            FOUR_OF_A_KIND,
+            find_kind2(&Vec::from(['0', '1', '1', '1', '1']))
         );
     }
 
     #[test]
     pub fn test_full_house() {
-        assert_eq!(FULL_HOUSE, find_kind(&Vec::from(['0', '0', '0', '1', '1'])));
-        assert_eq!(FULL_HOUSE, find_kind(&Vec::from(['0', '0', '1', '1', '1'])));
+        assert_eq!(
+            FULL_HOUSE,
+            find_kind2(&Vec::from(['0', '0', '0', '1', '1']))
+        );
+        assert_eq!(
+            FULL_HOUSE,
+            find_kind2(&Vec::from(['0', '0', '1', '1', '1']))
+        );
+        assert_eq!(
+            FULL_HOUSE,
+            find_kind2(&Vec::from(['*', '0', '0', '1', '1']))
+        );
+        assert_eq!(
+            FULL_HOUSE,
+            find_kind2(&Vec::from(['0', '0', '*', '1', '1']))
+        );
     }
 
     #[test]
     pub fn test_3of() {
         assert_eq!(
             THREE_OF_A_KIND,
-            find_kind(&Vec::from(['0', '0', '0', '1', '2']))
+            find_kind2(&Vec::from(['0', '0', '0', '1', '2']))
         );
         assert_eq!(
             THREE_OF_A_KIND,
-            find_kind(&Vec::from(['1', '0', '0', '0', '2']))
+            find_kind2(&Vec::from(['1', '0', '0', '0', '2']))
         );
         assert_eq!(
             THREE_OF_A_KIND,
-            find_kind(&Vec::from(['3', '2', '3', '0', '3']))
+            find_kind2(&Vec::from(['3', '2', '3', '0', '3']))
+        );
+
+        assert_eq!(
+            THREE_OF_A_KIND,
+            find_kind2(&Vec::from(['*', '1', '1', '2', '3']))
+        );
+        assert_eq!(
+            THREE_OF_A_KIND,
+            find_kind2(&Vec::from(['2', '1', '*', '1', '3']))
+        );
+        assert_eq!(
+            THREE_OF_A_KIND,
+            find_kind2(&Vec::from(['3', '2', '*', '1', '3']))
         );
     }
 
     #[test]
     pub fn test_1pair() {
-        assert_eq!(ONE_PAIR, find_kind(&Vec::from(['0', '0', '1', '2', '3'])));
-        assert_eq!(ONE_PAIR, find_kind(&Vec::from(['0', '1', '1', '2', '3'])));
-        assert_eq!(ONE_PAIR, find_kind(&Vec::from(['0', '1', '2', '2', '3'])));
-        assert_eq!(ONE_PAIR, find_kind(&Vec::from(['0', '1', '2', '3', '3'])));
+        assert_eq!(ONE_PAIR, find_kind2(&Vec::from(['0', '0', '1', '2', '3'])));
+        assert_eq!(ONE_PAIR, find_kind2(&Vec::from(['0', '1', '1', '2', '3'])));
+        assert_eq!(ONE_PAIR, find_kind2(&Vec::from(['0', '1', '2', '2', '3'])));
+        assert_eq!(ONE_PAIR, find_kind2(&Vec::from(['0', '1', '2', '3', '3'])));
     }
 
     #[test]
     pub fn test_2pair() {
-        assert_eq!(TWO_PAIR, find_kind(&Vec::from(['0', '0', '1', '1', '2'])));
-        assert_eq!(TWO_PAIR, find_kind(&Vec::from(['0', '0', '1', '2', '2'])));
-        assert_eq!(TWO_PAIR, find_kind(&Vec::from(['0', '1', '1', '2', '2'])));
+        assert_eq!(TWO_PAIR, find_kind2(&Vec::from(['0', '0', '1', '1', '2'])));
+        assert_eq!(TWO_PAIR, find_kind2(&Vec::from(['0', '0', '1', '2', '2'])));
+        assert_eq!(TWO_PAIR, find_kind2(&Vec::from(['0', '1', '1', '2', '2'])));
     }
 
     #[test]
     pub fn test1() {
         let test_data = test_data();
 
-        let mut cards = parse_cards(&test_data);
-        println!("{cards:?}");
-        cards.sort_by(|a, b| a.value.cmp(&b.value).then(a.compare_cards(b)));
-
-        println!("{cards:?}");
-
-        let mut answer = 0;
-        for (index, card) in cards.iter().enumerate() {
-            answer += ((index as u32 + 1) * card.bid);
-        }
+        let cards = parse_cards(&test_data);
+        let answer = sort_and_score(cards);
         assert_eq!(6440, answer);
+    }
+
+    #[test]
+    pub fn test2() {
+        let mut test_data = test_data();
+        // replace all the jacks with * so we can use the same code for puz1 and puz2
+        test_data = test_data.replace('J', "*");
+        let cards = parse_cards(&test_data);
+        let answer = sort_and_score(cards);
+        println!("{answer}");
+
+        assert_eq!(5905, answer);
     }
 
     fn test_data() -> String {
